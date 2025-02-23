12ad3a791b34b172f157512c5c82dd65c0d55dac
Tomek <tomaszrajda@gmail.com>
2025-02-23 19:36:17 +0100

    pipeline test, dev default

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace PipelineTest
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
